5512b54be1d026825ddad97036b9e8c096b4e467
unknown <seo970713@gmail.com>
2023-09-06 00:42:41 +0900

    엔티티 회원 이름 변경 및 단위 테스트

---
+++ src/main/java/com/example/fastcampusmysql/application/controller/MemberController.java
@@ -0,0 +1,36 @@
+package com.example.fastcampusmysql.application.controller;
+
+import com.example.fastcampusmysql.domain.member.dto.MemberDto;
+import com.example.fastcampusmysql.domain.member.dto.MemberNicknameHistoryDto;
+import com.example.fastcampusmysql.domain.member.dto.RegisterMemberCommand;
+import com.example.fastcampusmysql.domain.member.entity.Member;
+import com.example.fastcampusmysql.domain.member.service.MemberReadService;
+import com.example.fastcampusmysql.domain.member.service.MemberWriteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Tag(name = "회원정보")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/members")
+public class MemberController {
+    final private MemberWriteService memberWriteService;
+    final private MemberReadService memberReadService;
+
+    @PostMapping
+    public MemberDto register(@RequestBody RegisterMemberCommand command) {
+        var member = memberWriteService.register(command);
+        return memberReadService.toDto(member);
+    }
+
+    @GetMapping("/{id}")
+    public MemberDto getMember(@PathVariable Long id) {
+        return memberReadService.getMember(id);
+    }
+
+}
+
